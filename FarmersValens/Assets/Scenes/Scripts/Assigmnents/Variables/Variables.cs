476841b188292155cd55d2e9acd3180301a8411a
Josh <10923379@my.uvu.edu>
2020-01-15 17:18:25 -0700

    Worked on variables

--- FarmersValens/Assets/Scenes/Scripts/Assigmnents/Variables/Variables.cs
+++ FarmersValens/Assets/Scenes/Scripts/Assigmnents/Variables/Variables.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
+    //Declaration
+    public int cookieBox;
+    public float waterBottle;
+    private string firstName;
+    public double rbi;
+    public GameObject player; 
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+    public float amount;
+    public float money;
+    public float cost;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cookies(amount, money, cost);
     }
 
     // Update is called once per frame
@@ -15,4 +29,20 @@ public class Variables : MonoBehaviour
     {
         
     }
+
+
+    void Cookies(int amount, float money, float cost)
+    {
+        float total;
+
+        total = amount + cost - money;
+
+   
+    
+        Debug.Log(total);
+    }
+
+   
+
 }
+
